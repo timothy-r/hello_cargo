c4084b94a8bab11a0682e55f64ed537b8d036f30
timothy-r <tim.rodger@gmail.com>
2024-09-19 13:56:38 +0100

    final guessing game version

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+
+    println!("Guess the number between 1 and 1000 (inclusive)");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=1000);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read a line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter an integer");
+                continue;
+            }
+        };
+
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+
+    }
+}
